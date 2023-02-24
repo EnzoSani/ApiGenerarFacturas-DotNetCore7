2f64d8480c5bb229b0243ba529ff4828160c5503
EnzoSani <enzosanabria23@gmail.com>
2023-02-24 15:12:44 -0300

    Add UserToken Model

--- AppGenerarFacturas/Models/JwtSettings.cs
+++ AppGenerarFacturas/Models/JwtSettings.cs
@@ -3,13 +3,16 @@
     public class JwtSettings
     {
         public bool ValidateIssuerSigningKey { get; set; }
-        public string IssuerSigningKey { get; set; }
-        public bool ValidateIssuer { get; set; }
-        public string ValidIssuer { get; set; }
+        public string IssuerSigningKey { get; set; } = string.Empty;
+
+        public bool ValidateIssuer { get; set; } = true;
+        public string? ValidIssuer { get; set; }
+
         public bool ValidateAudience { get; set; }
-        public string ValidAudience { get; set; }
-        public bool ValidLifetime { get; set; }
-        public bool RequiredExpirationTime { get; set; }
+        public string? ValidAudience { get; set; }
+
+        public bool ValidLifetime { get; set; } = true;
+        public bool RequiredExpirationTime { get; set; } 
 
     }
 }
---
+++ AppGenerarFacturas/Models/UserTokens.cs
@@ -0,0 +1,15 @@
+﻿namespace AppGenerarFacturas.Models
+{
+    public class UserTokens
+    {
+        public int Id { get; set; }
+        public string Token { get; set; }
+        public string UserName { get; set; }
+        public TimeSpan Validity { get; set; }
+        public string RefreshToken { get; set; }
+        public string EmailId { get; set; }
+        public Guid GuidId { get; set; }
+        public DateTime ExpirateDateTime { get; set; }
+
+    }
+}
