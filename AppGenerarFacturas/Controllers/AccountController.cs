8d25f3fe6a94f43f6f350f58e48f209c1022f6ac
EnzoSani <enzosanabria23@gmail.com>
2023-03-02 16:14:33 -0300

    Add AccountController

---
+++ AppGenerarFacturas/Controllers/AccountController.cs
@@ -0,0 +1,50 @@
+﻿using AppGenerarFacturas.DataAccess;
+using AppGenerarFacturas.Helpers;
+using AppGenerarFacturas.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AppGenerarFacturas.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class AccountController : ControllerBase
+    {   
+        private readonly ApplicationDBContext _dbContext;
+        private readonly JwtSettings _jwtSettings;
+        public AccountController(ApplicationDBContext dbContext, JwtSettings jwtSettings)
+        {
+            _dbContext = dbContext;
+            _jwtSettings = jwtSettings;
+        }
+
+
+        [HttpPost]
+        public IActionResult GetToken(UserLogins userLogins)
+        {
+            try
+            {
+                var Token = new UserTokens();
+                var Valid = _dbContext.Users.Any(user => user.Name.Equals(userLogins.UserName, StringComparison.OrdinalIgnoreCase));
+
+                if (Valid)
+                {
+                    var User = _dbContext.Users.FirstOrDefault(user => user.Name.Equals(userLogins.UserName,StringComparison.OrdinalIgnoreCase));
+
+                    Token = JwtHelpers.GetTokenKey(new UserTokens()
+                    {
+                        UserName = User.Name,
+                        EmailId = User.EmailId,
+
+
+                    });
+                }
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+    }
+}
